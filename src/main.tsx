995fdb654c99d62048538decab83db2aa490d97b
Matteo Pietro Dazzi <matteo.dazzi@mia-platform.eu>
2022-08-31 18:36:32 +0200

    chore: additional config

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,39 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import {renderWithQiankun, qiankunWindow, QiankunProps} from 'vite-plugin-qiankun/dist/helper'
 import App from './App'
 import './index.css'
 
-ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-)
+const retrieveContainer = (props: QiankunProps) => props.container ?? document
+
+let root: ReactDOM.Root
+
+const render = (props: QiankunProps) => {
+  const container = retrieveContainer(props)
+  root = ReactDOM.createRoot(container.querySelector('#root') as HTMLElement)
+  root.render(
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  )
+}
+
+renderWithQiankun({
+  mount(props) {
+    render(props)
+  },
+  bootstrap() {
+    console.log('bootstrap')
+  },
+  unmount(props) {
+    root.unmount()
+  },
+  update() {
+    console.log('update')
+  },
+})
+
+if (!qiankunWindow.__POWERED_BY_QIANKUN__) {
+  render({})
+}
+
