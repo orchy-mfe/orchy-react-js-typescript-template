848a392c279d5f7a16a4e7bdf4448149ffc54908
Matteo Pietro Dazzi <matteo.dazzi@mia-platform.eu>
2022-09-12 11:21:52 +0200

    test increment

--- src/App.spec.tsx
+++ src/App.spec.tsx
@@ -1,5 +1,5 @@
 import {describe, it, expect} from 'vitest'
-import {render, screen} from '@testing-library/react'
+import {render, screen, fireEvent} from '@testing-library/react'
 
 import App from './App'
 
@@ -7,5 +7,20 @@ describe('App', () => {
     it('renders correctly', () => {
         render(<App/>)
         expect(screen.getByText('Click on the Vite and React logos to learn more')).toBeDefined()
+    })
+
+    it('count increments correctly', () => {
+        render(<App />)
+
+        const initialCountButton = screen.getAllByRole('button').at(0) as HTMLElement
+        expect(initialCountButton).toBeDefined()
+        expect(initialCountButton.innerHTML).toEqual('count is 0')
+
+        fireEvent(initialCountButton, new MouseEvent('click', {
+            bubbles: true,
+            cancelable: true,
+        }))
+
+        expect(initialCountButton.innerHTML).toEqual('count is 1')
     })
 })
